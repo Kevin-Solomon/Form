140c31233dc85a58fb5d76dc9675b467a75335a2
Kusum2000 <kusumrch@gmail.com>
2021-06-15 18:58:39 +0530

    now required photos get uploaded

--- src/display uploaded file.js
+++ src/display uploaded file.js
@@ -2,81 +2,84 @@ window.addEventListener("load",function(){
     //this file is to display the student details
     const submitbtn = document.getElementById('upload');
     submitbtn.addEventListener("click", function(e) {
-
-       //perform validation on the csv file uploaded 
-        var imgsUpload = document.getElementById("file2").files;
-                    //check if file is uploaded by user and supported by web browser
+        //check if file is uploaded by user and supported by web browser
         if (typeof (FileReader) != "undefined") {
-                
-            const mainDiv = document.getElementById("dvCSV");
-                if (imgsUpload) {
-                    [].forEach.call(imgsUpload, readAndPreview);
-                        
-                }
-                    
-                function readAndPreview(file) {
-    
-                        
-                        
-                    var reader = new FileReader();
-                    const stuDiv = document.createElement("div");
-                    stuDiv.id = file.name;
-                    stuDiv.className = "card";
+            var imgsUpload = document.getElementById("file2").files;    
+            var mainDiv = document.getElementById("dvCSV");
+            console.log("next read csv");
+            
+            var fileUpload = document.getElementById("file1");
+            var reader = new FileReader();
+            reader.onload = function(progressEvent) {
+                    //console.log(this.result);
+                    var file = this;
+                    readCSV(file);
+            };
+            reader.readAsText(fileUpload.files[0]);
+
+
+            function readCSV(file){
+                console.log("read csv");
+                var rows = file.result.split("\n");
+                for (var i = 1; i < rows.length-1; i++) {
+                    //file data split into rows
+                    var cells = rows[i].split(",");
+                       
+                    var stuDiv = document.createElement("div");
+                    stuDiv.className="card";
+                    stuDiv.id=cells[1].replace(/[\r]/g, "");
                     mainDiv.appendChild(stuDiv);
-                            
-                    console.log("file uploaded");
-                    reader.addEventListener("load", function () {
-                        var image = new Image();
-                        image.style.height = "170px";
-                        image.style.width = "130px"
-                        image.alt = image.id = file.name;
-                        image.src = this.result;
-                        stuDiv.appendChild( image );
-                            
-                        console.log(file.name);
-                    }, false);        
-                          reader.readAsDataURL(file);
-                          readCSV(stuDiv);
-               
-                
-                 
-        
-    
-                function readCSV(stuDiv){
-                    console.log("next read csv");
-                    var reader2 = new FileReader();
-                    var fileUpload = document.getElementById("file1");
+                    var stuDivid=stuDiv.id;
                     
-                    reader2.onload = function (e) {
-                        var rows = e.target.result.split("\n");
-                        for (var i = 1; i < rows.length-1; i++) {
-                            //file data split into rows
-                            var cells = rows[i].split(",");
-                            const divID = cells[1].replace("\r","");
-                            const stuDivID = stuDiv.id.split(".")[0];
-                            if(stuDivID===divID){
-                            //insert the student name & ID into the card
-                                for(var j=0; j<cells.length; j++){
-                                    console.log("working");
-                                    var p = document.createElement("p");
-                                    p.innerHTML=cells[j];
-                                    stuDiv.appendChild(p);
-                                    console.log(stuDiv);
-                                }
-                            }
-                        }
+                    console.log(stuDiv.id);
+                    
+                    for(var k=0;k<imgsUpload.length;k++){
+                        var imgname=imgsUpload[k].name.split(".")[0];
+                        if(stuDivid.localeCompare(imgname)===0){
+                            console.log(stuDivid,imgname);
+                            readAndPreview(stuDiv,imgsUpload[k],cells);     
+                        }     
+                    }
+                    
+                    //insert the student name & ID into the card
+                    
+                }
+            
+            }
+            function readAndPreview(stuDiv,file,cells) {
+
+                // Make sure `file.name` matches our extensions criteria
+                if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
+                  var reader = new FileReader();
+            
+                  reader.addEventListener("load", function () {
+                    var image = new Image();
+                    image.alt=stuDiv.id;
+                    image.id = file.name;
+                    image.src = this.result;
+                    
+                    stuDiv.appendChild( image );
+                    for(var j=0; j<cells.length; j++){
+                        console.log("working");
+                        var p = document.createElement("p");
+                        p.innerHTML=cells[j];
+                        stuDiv.appendChild(p);
+                        console.log(stuDiv);
                         
-                    };
-                    //calls function to read the csv file
-                    reader2.readAsText(fileUpload.files[0]);
+                    } 
+                  }, false);
+            
+                  reader.readAsDataURL(file);
                 }
-                
-            } 
+            
+            return stuDiv;
+            }
         }
-                else {
-                //error message for unsupported browsers
-                    alert("This browser does not support HTML5.");
-                }
+    
+        else {
+            //error message for unsupported browsers
+            alert("This browser does not support HTML5.");
+        }
             
         
     });
@@ -84,4 +87,3 @@ window.addEventListener("load",function(){
      
     
 });
-
